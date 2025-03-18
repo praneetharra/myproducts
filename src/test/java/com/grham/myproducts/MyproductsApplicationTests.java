0c90e79a5ef1c0732f8bbe149cc9824b105f7342
architakatipelly <125400084+architakatipelly@users.noreply.github.com>
2025-03-18 15:23:13 -0600

    Initial Commit

---
+++ src/test/java/com/grham/myproducts/MyproductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grham.myproducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyproductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
